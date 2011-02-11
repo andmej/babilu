9f72749247b7c39065df2e706d5e04959d87d0e3
Andrés Mejía <andmej@gmail.com>
2011-02-11 16:59:24 +0100

    Do not raise an exception if the generation process fails. It will always fail on Heroku!

--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
 require "babilu"
 I18n.reload!
-Babilu.generate
+begin
+  Babilu.generate
+rescue Exception => e
+  STDERR.puts "[ERROR] Exception raised while generating Babilu JavaScript file. Exception: #{e.inspect}"
+end
